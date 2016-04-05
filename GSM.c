29eb5745c2a1612778b6a2a001db481db7a8b51f
Ban Ananas <vturk@riteh.hr>
2016-04-05 15:58:14 +0200

    Verzija 5.4. - ?alje sms

--- GSM.c
+++ GSM.c
@@ -4,24 +4,24 @@
 #include <avr/pgmspace.h>
 #include <avr/interrupt.h>
 #include "lcd.h"
-#include <string.h>
-//ovo je proba LALALALA
+
 #define F_CPU	8000000UL
-			
 #define F_PWM	1000U
 #define N		8
 #define TOP		(((F_CPU/F_PWM)/N)-1)
 #define CONTRAST_DEF TOP/3
 
 #define BAUD 9600
-//#define BRC ((F_CPU/16/BAUD)-1) //BAUD PRESCALAR (for Asynch. mode) 
+#define BRC ((F_CPU/16/BAUD)-1) //BAUD PRESCALAR (for Asynch. mode) 
 
-#define BRC (((F_CPU / (BAUD * 16))) - 1)
+//#define BRC (((F_CPU / (BAUD * 16))) - 1)
+
 
 #define TX_BUFFER_SIZE 128
 #define RX_BUFFER_SIZE 128
-volatile char serialBuffer[TX_BUFFER_SIZE];
-volatile char rxBuffer[RX_BUFFER_SIZE];
+char serialBuffer[TX_BUFFER_SIZE];
+char rxBuffer[RX_BUFFER_SIZE];
+
 volatile uint8_t serialReadPos=0;
 volatile uint8_t serialWritePos=0;
 volatile uint8_t rxReadPos=0;
@@ -29,53 +29,30 @@ volatile uint8_t rxWritePos=0;
 
 void UART_Init(void){
 	/* Set baud rate*/
-	UBRRH = (BRC>>8);
+	UBRRH = 0;
 	UBRRL = BRC;
 	/*Enable reciver and transmitter*/
-	UCSRB = (1 << RXEN) | (1 << TXEN)| (1<<RXCIE)| (1<<TXCIE); // interrupts
+	UCSRB = (1 << RXEN) | (1 << TXEN)| (1<<RXCIE)| (1<<TXCIE);//|(1<<UDRIE); // interrupts
 	/*set frame format: 8 bit*/
 	
-	UCSRC =  (1 << UCSZ0) | (1 << UCSZ1); // Character Size 8 bit?
+	UCSRC = (1 << UCSZ0) | (1 << UCSZ1)|(1 << URSEL); // Character Size 8 bit?
 	/*UPM1 UPM0,  even-1 0 odd- 1 1 */
+	//UCSRC = _BV(URSEL) | _BV(UCSZ0) | _BV(UCSZ1) ;
 }
 
-
-void appendSerial(char c){
+ISR(USART_UDRE_vect){
 	
-	//lcd_puts("a");
-	serialBuffer[serialWritePos]=c;	
-	serialWritePos++;
-	
-	if(serialWritePos>=TX_BUFFER_SIZE)
-	{
-	serialWritePos=0;
-	}	
-}
-
-void serialWrite(char c[]){
-	
-	for(uint8_t i = 0;i <strlen(c);i++)
-	{
-		appendSerial(c[i]);
-	
-	}
-		
-		if(UCSRA & (1<<UDRE))
-		{
-			UDR=0;
-		}
-
+	lcd_puts_P("empty");
 }
 
 ISR(USART_TXC_vect){
 	
+	//lcd_puts("TX");
 	if(serialReadPos != serialWritePos)
 	{
 		//char c= serialBuffer[serialReadPos];
 		//lcd_puts(c);
-		UDR = serialBuffer[serialReadPos];
-		lcd_puts (serialBuffer[serialReadPos]);
-		//lcd_puts_P("TX");
+		UDR = serialBuffer[serialReadPos];		
 		serialReadPos++;
 		
 		if (serialReadPos>=TX_BUFFER_SIZE)
@@ -83,13 +60,19 @@ ISR(USART_TXC_vect){
 			serialReadPos=0;
 		}
 	}
-
+	
+	
+	
 }
+
 ISR(USART_RXC_vect){
 	
-	rxBuffer[rxWritePos]= UDR;
-	lcd_puts_P("RX");
-		
+	//lcd_puts("RX");
+	lcd_putc(UDR);
+	rxBuffer[rxWritePos] = UDR;
+	
+	//lcd_puts(UDR);
+	//lcd_putc (rxBuffer[rxWritePos]);
 	rxWritePos++;
 	
 	if(rxWritePos>=RX_BUFFER_SIZE){
@@ -99,7 +82,7 @@ ISR(USART_RXC_vect){
 
 
 char peekChar(void){
-	char ret= '\0';
+	 char ret= '\0';
 	
 	if(rxReadPos!=rxWritePos){
 		
@@ -114,7 +97,7 @@ char getChar(void){
 	char ret= '\0';
 	if(rxReadPos!=rxWritePos){
 		
-		ret= rxBuffer[rxWritePos];
+		ret = rxBuffer[rxWritePos];
 		rxReadPos++;
 		
 		if(rxReadPos<=RX_BUFFER_SIZE){
@@ -125,18 +108,26 @@ char getChar(void){
 	return ret;
 }
 
-unsigned char USART_getc (void) {
-	while (!(UCSRA & _BV(RXC)));
-	lcd_puts_P("get");
-	return UDR;
+volatile unsigned char USART_getc( void )
+{
 	
+// Wait for data to be received
+while ( !(UCSRA & _BV(RXC)) );
+	//lcd_puts_P("0");
+// Get and return received data from buffer
+return UDR;
+
 }
 
-void USART_putc (unsigned char data) {
-	while (!(UCSRA & _BV(UDRE)));
+
+
+void USART_putc (char data) {
+// Wait for empty transmit buffer
+	while ( !( UCSRA & _BV(UDRE)) );
+	// Put data into buffer, i.e., send the data
 	UDR = data;
-	lcd_puts_P("put");
 }
+
 
 
 static void IO_Init(void)
@@ -149,7 +140,15 @@ static void IO_Init(void)
    // lcd_puts_P("Hello world\n");	
 }
 
+void USART_puts( char str[]){
+	int i;
 
+	for(i=0;i<strlen(str);i++){
+		USART_putc(str[i]);
+		_delay_ms(1000);
+		
+	}		
+}
 
 void main(void)
 {
@@ -157,65 +156,46 @@ void main(void)
 	UART_Init();	
 
 	sei();
-	/*	
-	if (bit_is_set(UCSRA,TXC))
-	{
-		lcd_puts_P("TXCset");
-	}
+	//potreban delay!!
+	char AT[] = "AT"; // 4 znamenke potrebne
+	char text_mode[] = "AT+CMGF=1";
 	
-	UDR = 'A';
+	char sms[]="AT+CMGS=";
+	char num ="00385919390809";
+	char sms_text[]="<3";
+	
+	//USART_putc(65);
+	//_delay_ms(1000);
+	//USART_putc(84);
+	//_delay_ms(1000);
+	//USART_putc(13);
+	
+	USART_puts(text_mode);
+	USART_putc(13);
+	
+	USART_puts(sms);
+	USART_putc(34);
+	USART_puts(num);
+	USART_putc(34);
 	_delay_ms(1000);
-
+	USART_putc(13);
 	
-	UDR = 'T';
-	_delay_ms(1000);	
-	
-	UDR = 'E';
-	_delay_ms(1000);
-	
-	char c = UDR;
-	*/
-	
-	char AT[] = "abcdefgh";
-	char text_mode[] = "AT + CMGF = 1";
-	char sms[]="AT+CMGS='00385996834050'+SMS";
-	char charater_mode[] = "AT + CMCS = 'GSM'";
-	
-//	UDR = "AT + CMGF = 1";
-	
-//	USART_putc(AT);
-//	USART_putc(text_mode);
-//	USART_putc(charater_mode);
-	
-
-	serialWrite(AT);
-//	serialWrite(0x0D); //enter after AT!
+	USART_puts(sms_text);
+	USART_putc(26);// CTRL+z
+	USART_putc(13);
+		
+	//char c= USART_getc();
 	
 	//USART_putc(AT);
-	//USART_putc(0x0D);
-	_delay_ms(200);
-		
-	if (bit_is_set(UCSRA,TXC))
-	{
-		lcd_puts_P("TXCset");
-	}
+	//USART_putc("A");
+	//USART_putc("T");
 	
-	//char c= USART_getc();
-	char c= getChar();
-	
+
 	_delay_ms(200);
 	
 
 	
-	if (bit_is_set(UCSRA,RXC))
-	{
-		lcd_puts_P("RXCset");
-	}
-	
-	while(1){
-	
-	}
-	
+	while(1){}
 	
 } 
 
