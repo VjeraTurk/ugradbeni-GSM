4e3cbf3e11133486edc4ca90762985df279179b9
Vjera Turk <vturk@riteh.hr>
2016-04-08 18:26:27 +0000

    GSM.c edited online with Bitbucket Neznam sta je al mi SourceTree ne prepoznaje izmjene- pokusat cu popraviti do iduceg commitanja

--- GSM.c
+++ GSM.c
@@ -18,7 +18,7 @@
 
 
 #define TX_BUFFER_SIZE 128
-#define RX_BUFFER_SIZE 128
+#define RX_BUFFER_SIZE 255
 char serialBuffer[TX_BUFFER_SIZE];
 char rxBuffer[RX_BUFFER_SIZE];
 
@@ -65,13 +65,56 @@ ISR(USART_TXC_vect){
 	
 }
 
+char numm[13]; 
+uint8_t i=4;
+uint8_t over=0;
+uint8_t tm=0;
+volatile uint8_t f=0;
+
 ISR(USART_RXC_vect){
 	
 	//lcd_puts("RX");
-	lcd_putc(UDR);
-	rxBuffer[rxWritePos] = UDR;
 	
-	//lcd_puts(UDR);
+	rxBuffer[rxWritePos] = UDR;
+	rxBuffer[rxWritePos+1] = '/0';
+	
+	if(rxBuffer[rxWritePos]=='5'){
+		if(rxBuffer[rxWritePos-1]=='8'){
+			f=1;
+			/*
+			numm[3] = rxBuffer[rxWritePos];
+			numm[2] = rxBuffer[rxWritePos-1];
+			numm[1] = rxBuffer[rxWritePos-2];
+			numm[0] = rxBuffer[rxWritePos-3];
+			numm[0] = rxBuffer[rxWritePos-4];
+			*/
+		numm[0]='+';
+		numm[1]='3';
+		numm[2]='8';
+		i=3;
+				
+		}
+	}
+	
+	if(f==1 && i<13 ){
+		numm[i]=rxBuffer[rxWritePos];
+		//lcd_putc(numm[i]);
+		i++;
+	}
+	if(rxBuffer[rxWritePos]=='K' && rxBuffer[rxWritePos-1]=='O'){
+		
+		lcd_puts("ignor");
+		tm=1;
+	}
+	
+	if( tm==1 && rxBuffer[rxWritePos]=='K' && rxBuffer[rxWritePos-1]=='O'){
+		
+		lcd_puts("gotovo");
+		over=1;
+		tm=0;
+	}
+	
+	//lcd_putc(UDR);
 	//lcd_putc (rxBuffer[rxWritePos]);
 	rxWritePos++;
 	
@@ -80,17 +123,6 @@ ISR(USART_RXC_vect){
 	}
 }
 
-
-char peekChar(void){
-	 char ret= '\0';
-	
-	if(rxReadPos!=rxWritePos){
-		
-		ret= rxBuffer[rxWritePos];
-		}
-	return ret;
-	
-}
 
 char getChar(void){
 	
@@ -159,20 +191,16 @@ void main(void)
 	//potreban delay!!
 	char AT[] = "AT"; // 4 znamenke potrebne
 	char text_mode[] = "AT+CMGF=1";
-	
+///SMS	
 	char sms[]="AT+CMGS=";
 	char num ="00385919390809";
-	char sms_text[]="<3";
+	//char sms_text[30];
 	
-	//USART_putc(65);
-	//_delay_ms(1000);
-	//USART_putc(84);
-	//_delay_ms(1000);
-	//USART_putc(13);
-	
+//TEXT MODE
 	USART_puts(text_mode);
 	USART_putc(13);
 	
+	/*
 	USART_puts(sms);
 	USART_putc(34);
 	USART_puts(num);
@@ -183,19 +211,68 @@ void main(void)
 	USART_puts(sms_text);
 	USART_putc(26);// CTRL+z
 	USART_putc(13);
+	*/
+/// READ MESSAGE
+	
+	//zahtjev za poruku  s indexom 1
+	char request[]="at+cmgr=1";	
+	char number[9];
+	
+	USART_puts(request);
+	USART_putc(13);
+	_delay_ms(5000);
+	
+
+	char *p; /**< Pointer used for sliding through an array of chars */
+	
+	rxWritePos=0;
+	/*
+	if (strstr(rxBuffer, "+385")) {
+		p = strstr(rxBuffer, "+385"); 
+		p = strchr(p, "3");
+		//lcd_puts(p);
+		strncpy(number, p, 9);
+	}
+	*/
+	lcd_puts(numm);
+	
+	while(1){
 		
-	//char c= USART_getc();
+		if(over){
+			
+				lcd_puts("temp");
+				over=0;
+			
+		
+		if (strstr(rxBuffer, "Temperatura?")) {
+			lcd_gotoxy(0,1);
+			lcd_puts("vvvvv");
+			char sms_text[]="Trenutna temperatura: ** °C";
+			
+			lcd_puts(sms);	
+			USART_puts(sms);
+			
+			lcd_putc(34);
+			USART_putc(34);
+			lcd_puts(numm);
+			USART_puts(numm);
+			
+			lcd_putc(34);
+			USART_putc(34);
+			_delay_ms(1000);
+			
+			lcd_putc(13);
+			USART_putc(13);
+			lcd_puts(sms_text);	
+			USART_puts(sms_text);
+			
+			USART_putc(26);// CTRL+z
+			USART_putc(13);
 	
-	//USART_putc(AT);
-	//USART_putc("A");
-	//USART_putc("T");
-	
-
-	_delay_ms(200);
-	
-
-	
-	while(1){}
+			lcd_puts("poslano");
+		}
+		}
+	}
 	
 } 
 
