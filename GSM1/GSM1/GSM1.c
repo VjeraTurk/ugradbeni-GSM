af45021b357ccd7f500272db1bef5a7a2405b62f
Vjera Turk <vturk@riteh.hr>
2016-04-21 21:03:00 +0200

    -?alje  GSMu request za porukom indexa 1svakih 5 sekundi - za prvu primljenu poruku ispisuje na LCD intenzitet i dan/noc - za svaku iducu spajanje stringova pogre?no "IS: 220 dan 220 dan"

--- GSM1/GSM1/GSM1.c
+++ GSM1/GSM1/GSM1.c
@@ -1,12 +1,15 @@
+#define F_CPU	8000000UL
 #include <stdlib.h>
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/pgmspace.h>
 #include <avr/interrupt.h>
+#include <string.h>
+
 #include "lcd.h"
 
-#define F_CPU	8000000UL
-#define F_PWM	1000U
+
+#define F_PWM	1000Ubh
 #define N		8
 #define TOP		(((F_CPU/F_PWM)/N)-1)
 #define CONTRAST_DEF TOP/3
@@ -18,7 +21,7 @@
 //proba
 
 #define TX_BUFFER_SIZE 128
-#define RX_BUFFER_SIZE 255
+#define RX_BUFFER_SIZE 128
 char serialBuffer[TX_BUFFER_SIZE];
 char rxBuffer[RX_BUFFER_SIZE];
 
@@ -65,9 +68,9 @@ ISR(USART_TXC_vect){
 	
 }
 
-char numm[13]; 
-uint8_t i=4;
-uint8_t over=0;
+volatile char numm[13]; 
+volatile uint8_t i=4;
+volatile uint8_t over=0;
 uint8_t tm=0;
 volatile uint8_t f=0;
 
@@ -76,7 +79,7 @@ ISR(USART_RXC_vect){
 	//lcd_puts("RX");
 	
 	rxBuffer[rxWritePos] = UDR;
-	rxBuffer[rxWritePos+1] = '/0';
+	//rxBuffer[rxWritePos+1] = '/0';
 	
 	if(rxBuffer[rxWritePos]=='5'){
 		if(rxBuffer[rxWritePos-1]=='8'){
@@ -115,40 +118,12 @@ ISR(USART_RXC_vect){
 	}
 	
 	//lcd_putc(UDR);
-	//lcd_putc (rxBuffer[rxWritePos]);
+	lcd_putc (rxBuffer[rxWritePos]);
 	rxWritePos++;
 	
 	if(rxWritePos>=RX_BUFFER_SIZE){
 		rxWritePos=0;
 	}
-}
-
-
-char getChar(void){
-	
-	char ret= '\0';
-	if(rxReadPos!=rxWritePos){
-		
-		ret = rxBuffer[rxWritePos];
-		rxReadPos++;
-		
-		if(rxReadPos<=RX_BUFFER_SIZE){
-			rxReadPos=0;
-		}
-	}
-	
-	return ret;
-}
-
-volatile unsigned char USART_getc( void )
-{
-	
-// Wait for data to be received
-while ( !(UCSRA & _BV(RXC)) );
-	//lcd_puts_P("0");
-// Get and return received data from buffer
-return UDR;
-
 }
 
 
@@ -182,24 +157,77 @@ void USART_puts( char str[]){
 	}		
 }
 
-void main(void)
+volatile char buffer[4];
+void ADC_Init(){
+	
+
+	//adc enable, prescaler=64 -> clk=115200
+	ADCSRA = _BV(ADEN)|_BV(ADPS2)|_BV(ADPS1);
+
+	//AVcc reference voltage
+	ADMUX = _BV(REFS0);
+}
+volatile char *getLight(uint8_t channel)
+{
+	//choose channel
+	ADMUX &= ~(0x7);
+	ADMUX |= channel;
+	ADMUX |= 1<<ADLAR;
+	
+	//start conversion
+	ADCSRA |= _BV(ADSC);
+
+	//wait until conversion completes
+	while (ADCSRA & _BV(ADSC) );
+	
+	 char ispis[10];
+	
+	
+	strcpy(ispis," ");
+// 	for (i = 0; i < 10; i++) {
+// 		ispis[i]='\0';
+// 	}
+// 	
+	*ispis=itoa(ADCH,ispis,10);
+	
+			if (ADCH<200)
+			{
+				lcd_clrscr();
+				//strcpy(ispis, "noc");
+				strcat(ispis, " noc");
+			}
+			else
+			{
+				lcd_clrscr();
+				strcat(ispis, " dan");
+				
+			}
+			_delay_ms(750);
+		
+
+	
+	return ispis;
+}
+int main(void)
 {
 	IO_Init();
-	UART_Init();	
+	UART_Init();
+	ADC_Init();	
 
 	sei();
+	
 	//potreban delay!!
 	char AT[] = "AT"; // 4 znamenke potrebne
 	char text_mode[] = "AT+CMGF=1";
 ///SMS	
 	char sms[]="AT+CMGS=";
-	char num ="00385919390809";
+	char *num ="00385919390809";
 	//char sms_text[30];
 	
 //TEXT MODE
 	USART_puts(text_mode);
 	USART_putc(13);
-	
+	_delay_ms(1000);
 	/*
 	USART_puts(sms);
 	USART_putc(34);
@@ -215,16 +243,16 @@ void main(void)
 /// READ MESSAGE
 	
 	//zahtjev za poruku  s indexom 1
+		//lcd_puts("blabla");
 	char request[]="at+cmgr=1";	
 	char number[9];
 	
-	USART_puts(request);
-	USART_putc(13);
-	_delay_ms(5000);
 	
 
-	char *p; /**< Pointer used for sliding through an array of chars */
-	
+// 	USART_puts(request);
+// 	USART_putc(13);
+// 	_delay_ms(5000);
+// 	
 	rxWritePos=0;
 	/*
 	if (strstr(rxBuffer, "+385")) {
@@ -234,46 +262,80 @@ void main(void)
 		strncpy(number, p, 9);
 	}
 	*/
-	lcd_puts(numm);
+	//lcd_puts(numm);
 	
 	while(1){
 		
+	 	USART_puts(request);
+	 	USART_putc(13);
+		 _delay_ms(5000);
+		
+
+		
 		if(over){
 			
-				lcd_puts("temp");
+		
 				over=0;
 			
+	
+			if (strstr(rxBuffer, "LUX?")) {
+				
+				lcd_gotoxy(0,1);
+				char *sms_text="IS: ";
+			
+				volatile char *rez= getLight(1);
+				
+				char *sms_full_text=strcat(sms_text,rez);
+				
+				lcd_puts(sms_full_text);	
+	/*
+				lcd_puts(sms);	
+				USART_puts(sms);
+			
+				lcd_putc(34);
+				USART_putc(34);
+				lcd_puts(numm);
+				USART_puts(numm);
+			
+				lcd_putc(34);
+				USART_putc(34);
+				_delay_ms(1000);
+			
+				lcd_putc(13);
+				USART_putc(13);
+				lcd_puts(sms_text);	
+
+				USART_puts(sms_text);
+			
+				USART_putc(26);// CTRL+z
+				USART_putc(13);
+				
+	*/			
+				lcd_gotoxy(0,1);
+				lcd_puts_P("poslano");
+			
+				char delete[]="at+cmgd=1";
+				USART_puts(delete);
+				USART_putc(13);
+				_delay_ms(3000);
+				int i;
+				for (i=0; i < 128; i++) {
+					rxBuffer[i] = '0';		
+				}
+				
+			
+				over=0;
+			
+				
+				
+			}else{
+				
+			lcd_puts("Nema poruke");
+			}
+		}
 		
-		if (strstr(rxBuffer, "Temperatura?")) {
-			lcd_gotoxy(0,1);
-			lcd_puts("vvvvv");
-			char sms_text[]="Trenutna temperatura: ** °C";
-			
-			lcd_puts(sms);	
-			USART_puts(sms);
-			
-			lcd_putc(34);
-			USART_putc(34);
-			lcd_puts(numm);
-			USART_puts(numm);
-			
-			lcd_putc(34);
-			USART_putc(34);
-			_delay_ms(1000);
-			
-			lcd_putc(13);
-			USART_putc(13);
-			lcd_puts(sms_text);	
-			USART_puts(sms_text);
-			
-			USART_putc(26);// CTRL+z
-			USART_putc(13);
-	
-			lcd_puts("poslano");
+		
+		
 		}
-		}
-	}
-	
+	return 0;
 } 
-
-
