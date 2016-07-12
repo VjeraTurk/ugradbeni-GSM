cb15e975865440795ab2111c288b96d4aa2ff1d9
Vjera Turk <vturk@riteh.hr>
2016-07-12 21:02:10 +0200

    - funkcije opisane u fromatu koji bi trebao odgovarati doksygenu za kreiranje dokumentacije, ra?eno prema GPS projektu

--- GSM1/GSM1/GSM1.c
+++ GSM1/GSM1/GSM1.c
@@ -1,3 +1,11 @@
+/**
+ * @file GSM1.c
+ * @author Lovro Speti?, vjera Turk, Patricija Zubali?
+ * @date 12 Jul 2016
+ * @brief This is the main file where AT commands are sent to gsm module requesting received SMS messages saved on SIM card within GSM module.
+ * SMS messages are being analyzed and replayed to with light intensity and light is switched off and on depending on SMS content.
+ */
+
 #define  F_CPU           8000000UL
 
 #include <stdlib.h>
@@ -9,7 +17,7 @@
 
 #include "lcd.h"
 
-#define F_PWM           1000U //bh
+#define F_PWM           1000U 
 #define N               8
 #define TOP               (((F_CPU/F_PWM)/N)-1)
 #define RED_DEF    TOP/3
@@ -17,7 +25,7 @@
 #define BAUD            9600
 #define BRC             ((F_CPU/16/BAUD)-1) //BAUD PRESCALAR (for Asynch. mode)
 #define TX_BUFFER_SIZE  128
-#define RX_BUFFER_SIZE  255 //255
+#define RX_BUFFER_SIZE  255 
 
 #define RED		PD4
 #define GREEN	PD3
@@ -26,7 +34,14 @@
 #define LED	PA2
 #define SLEEP	PA3
 
+//3 state flags: 0 - command not sent,  1 - command sent, 2 - OK answer received form GSM
+volatile uint8_t tm_flag=0; 
+volatile uint8_t rq_flag=0; 
+volatile uint8_t del_flag=0; 
 
+volatile int sms_flag=0;
+
+/////// U A R T communication ////////////
 char txBuffer[TX_BUFFER_SIZE];
 char rxBuffer[RX_BUFFER_SIZE];
 
@@ -34,56 +49,12 @@ volatile uint8_t txReadPos=0;
 volatile uint8_t txWritePos=0;
 volatile uint8_t rxReadPos=0;
 volatile uint8_t rxWritePos=0;
-
-volatile uint8_t tm_flag=0; //text mode flag: 1 - request sent 2 - OK answer received
-volatile uint8_t rq_flag=0; //request flag: 1 - command sent 2 - OK answer received
-volatile uint8_t del_flag=0; //delete flag: 1 - command sent 2 - OK answer received
-//proba
-
-volatile int sms_flag=0;
-//initialize IO (LCD), UART, ADC, RED LED
-void init(){
-	//initialize IO
-	lcd_init(LCD_DISP_ON);
-	lcd_clrscr();
-	//backlight pins
-	DDRD |=_BV(RED) | _BV(GREEN) | _BV(BLUE); //RGB PB2 PB1 PB0
-	PORTD|=_BV(RED) | _BV(GREEN) | _BV(BLUE);
-	
-	
-	//initialize UART
-	//set baud rate
-	UBRRH = 0;
-	UBRRL = BRC;
-	//enable receiver and transmitter
-	UCSRB = (1 << RXEN) | (1 << TXEN)| (1<<RXCIE)| (1<<TXCIE);
-	//set frame format: 8 bit, character size 8 bit
-	UCSRC = (1 << UCSZ0) | (1 << UCSZ1)|(1 << URSEL);
-	
-	//initialize ADC
-	//adc enable, prescaler=64 -> clk=115200
-	ADCSRA = _BV(ADEN)|_BV(ADPS2)|_BV(ADPS1);
-	//AVcc reference voltage
-	ADMUX = _BV(REFS0);
-
-	//PA2 RED LED
-	DDRA |= _BV(LED); //LED
-}
-//USART serial communication functions and interrupts
-ISR(USART_TXC_vect)
-{
-	if(txReadPos != txWritePos)
-	{
-		UDR = txBuffer[txReadPos];
-		//lcd_putc(txBuffer[txReadPos]);
-		txReadPos++;
-
-		if (txReadPos>=TX_BUFFER_SIZE)
-		{
-			txReadPos=0;
-		}
-	}
-}
+/**
+ * @brief USART Interrupt service routine which fills rxBuffer the moment new data is received
+ * 
+ * Depending on which flag is previously set to '1', it knows what the received content OK represents (an answer to which AT command: request, delete, text mode)
+ * It then sets flag to '2', enabling actions that follow after the return from the interrupt service routine. 
+ */
 ISR(USART_RXC_vect)
 {
 	rxBuffer[rxWritePos] = UDR;
@@ -113,6 +84,21 @@ ISR(USART_RXC_vect)
 		rxWritePos=0;
 	}
 }
+ISR(USART_TXC_vect)
+{
+	if(txReadPos != txWritePos)
+	{
+		UDR = txBuffer[txReadPos];
+		//lcd_putc(txBuffer[txReadPos]);
+		txReadPos++;
+
+		if (txReadPos>=TX_BUFFER_SIZE)
+		{
+			txReadPos=0;
+		}
+	}
+}
+
 void USART_putc (char data)
 {
 	// Wait for empty transmit buffer
@@ -132,67 +118,123 @@ void USART_puts(char str[])
 		_delay_ms(500);
 	}
 }
-void refresh_rxBuffer(){
-	*rxBuffer='\0';
-	rxReadPos=0;
-	rxWritePos=0;
+//////////////////////////////////////////
+
+/**
+ * @brief A function for initializing LCD, ADC and IO pins
+ * 
+ */
+void init();
+/**
+ * @brief This function sends at command for entering text mode. It is called in while loop
+ *	while(!enable_text_mode()); until OK answer is received from GSM, meaning it has entered the text mode.
+ *
+ * @param number[] phone number to send sms to
+ * @param sms_text[] text to send
+ * @return 0 or 1 depending on gsm answer (if answer is OK return is 1 else 0)
+ */
+int enable_text_mode();
+/**
+ * @brief This function sends at command for requesting sms of certain index.
+ * For indexes higher than 9, it also sends out index's tenner. 
+ * (The highest index it can request is 99, but since SIM car memory can handle around 25 messages it is not required)
+ *
+ * @param index 
+ * @param tenner
+ */
+void request_sms(char index, char tenner);
+/**
+ * @brief Function analyzes  what is written in rxBuffer after GSM finishes responding to AT command (requesting SMS message).
+ * There are 3 answers it is looking for:
+ * 1)	+CME: ERROR 321
+ * 2)	+CMGR: "REC UNREAD","0","<phone number>",,"yy/MM/dd,hh:mm:ss±zz"
+ * <sms text>
+ *
+ * OK
+ *
+ * 3)	+CMGR: "REC READ","0","<phone number>",,"yy/MM/dd,hh:mm:ss±zz"
+ * <sms text>
+ *
+ * OK
+ * 1) There is no message with this index.
+ * 2) There is UNREAD message, meaning phone number starts at position 24 of response.
+ * 3) There is READ message, meaning phone number starts at position 22 of response (UN ads 2 more characters before phone number).
+ * (This case is rare but covered. It would occur if message was not deleted or responded to the first time it is read)
+ *
+ * 4) If other errors (CME and CMS) and booting lines (which start with +SIND) repeat are received request is repeated (return 0) after awhile.
+ * Global variable int sms_flag is set if UNREAD or READ message is found.
+ *
+ * @return 1 if there is no message on requested index or if there is
+ * @return 0 if the error has occurred and request has to be repeated
+ */
+int read_rxBuffer();
+/**
+ * @brief Function extracts phone number from rxBuffer, after it is confirmed it containes a valid READ or UNREAD message in read_rxBuffer() function
+ * @param read
+ * read==1 meaning message is UNREAD and phone number starts at position 24 
+ * read==2 meaning message is READ and phone number starts at position 22
+ * It is assumed phone number has 12 digits. 385yy xxx xx xx
+ * Knowing on which position phone number starts, it extracts phone number from rxBuffer
+ * and saves it to global variable char from_phone_number.
+
+*/
+/*
+dodati novu provjeru umjesto trenutne za tocnije rezultate!:
+while(isdigit(rxBuffer[i])){
+	from_number[j]=rxBuffer[i];
+	j++;
+	i++;
 }
-void see_rxBuffer(){
+I drugacije odrediti digit_pos (npr. tražiti ")
+*/
+int get_from_number(int);
+volatile char from_number[13];
 
-	lcd_clrscr();
-	rxReadPos=0;
-	
-	while(rxReadPos!=rxWritePos){
-		//if(rxReadPos%16==0 || rxReadPos==0) lcd_gotoxy(0,0);
-		if (rxReadPos==0) lcd_gotoxy(0,0);
-		else if (rxReadPos==16 || rxReadPos==48) lcd_gotoxy(0,1);
-		else if (rxReadPos==32 || rxReadPos==64) {
-			lcd_clrscr();
-			lcd_gotoxy(0,0);
-		}
-		lcd_putc(rxBuffer[rxReadPos]);
-		rxReadPos++;
-		_delay_ms(175);
-	}
-}
-void send_sms(char number[], char sms_text[]){
-	
-	char AT_send_sms[]="AT+CMGS=";
-	
-	refresh_rxBuffer();
-	
-	USART_puts(AT_send_sms);
-	USART_putc(34);
-	USART_puts(number);
-	USART_putc(34);
-	_delay_ms(1000);
-	USART_putc(13);
-	_delay_ms(3000);
-	see_rxBuffer();
-	_delay_ms(3000);
-	USART_puts(sms_text);
-	USART_putc(26); // CTRL+z
-	_delay_ms(3000);
-	
-	see_rxBuffer();
-}
-void _4_sms_test(){
+/** 
+ * @brief If sms_flag is set to 2, meaning there is an SMS in rxBuffer, LUX()function is entered from main.
+ * It seeks code word "LUX?" in received SMS. 
+ * If code word "LUX?" is found getLight() function mesures light intensity
+ * and returns value between 1-255. Along with "dan" or "noc" (day(>200) or night(<200)) this value is send back via SMS.
+ *
+ * If the light is off during night, it also sends a question if you would like to turn the light on.
+ * If the light is on during daylight, it sends a question if you would like to turn the light off.
+ * If the next message it receives is "DA" (yes) it switches the light off or on.
+ */
+void LUX();
+volatile uint8_t getLight(uint8_t);
 
-	char p_sms[]="AT+CMGS=";
-	//char p_number[]="385976737211";
-	char p_number[]="385996834050";
+volatile uint8_t upaljeno = 0;
+volatile char from_number_lux[13];
 
-	char p_text[]="LUX?";
-	char p_text2[]="LUKS?";
-	char p_text3[]="DUGA?";
-	char p_text4[]="DA";
+/**
+ * @brief This function sets rxReadPos and rxWritePos to 0 and "empties" rxBuffer
+ *
+ * It is used after end of analyzing rxBuffer content to empty the buffer which contains received data. Every new received answer is the only answer in rxBuffer.
+ * Buffer always contains only last received answer form GSM. All tough rxBuffer is circular, 
+ * in normal situations, it's full capacity and overflow are never reached because of use of this function. 
+ *
+ */
+void refresh_rxBuffer();
+/**
+ * @brief This function puts rxBuffer content to 16x2 LCD screen
+ *
+ */
+void see_rxBuffer();
+
+void delete_sms(char, char);
+/**
+ * @brief This function is for sending sms containing given text to given number
+ *
+ * @param number[] phone number to send sms to
+ * @param sms_text[] text to send
+ */
+void send_sms(char number[], char sms_text[]);
+void _4_sms_test();
 
 
-	send_sms(p_number,p_text);
-	send_sms(p_number,p_text2);
-	send_sms(p_number,p_text3);
-	send_sms(p_number,p_text4);
-}
+void rainbow();
+void reboot();
+void sleep_mode();
 
 
 void date_time_check(){
@@ -209,7 +251,6 @@ void date_time_check(){
 	_delay_ms(1000);
 	see_rxBuffer();
 }
-
 void RED_light(){
 	PORTD |=_BV(RED); //RED ON
 	
@@ -229,108 +270,13 @@ void GREEN_light(){
 	PORTD &=~_BV(BLUE);
 }
 
-int enable_text_mode();
-void request_sms(char index, char tenner);
-int read_rxBuffer();
-
-int get_from_number(int);
-volatile char from_number[13];
-
-volatile uint8_t getLight(uint8_t);
-
-void LUX();
-volatile uint8_t upaljeno = 0;
-volatile char from_number_lux[13];
-
-
-void delete_sms(char, char);
-void reboot();
-void sleep_mode();
-//“ATD*100#” -> za poziv
-
-//pink: blue+ pwm red TOP/2 (Barbie) ili TOP/1.1 (fuksija)
-//purple: blue+ pwm red TOP/3.5
-//yello: green+ pwm red TOP/1.5
-//orange: green + pwm red TOP/1.2 ili 1.1
-
-
-void rainbow(){
-	
-	cli();
-	
-	TCCR1A |= _BV(COM1B1)|_BV(WGM11)|_BV(WGM10);
-	TCCR1B |= _BV(WGM13)|_BV(WGM12);
-	OCR1A   = TOP;				// set TOP value 
-	OCR1B   = RED_DEF;		// set default contrast value
-	
-	TIMSK  |= _BV(OCIE1A);
-
-	TCCR1B |= _BV(CS11) | _BV(CS10);
-	
-	RED_light();		//RED
-	_delay_ms(1000);
-	
-	GREEN_light();
-	OCR1B   = TOP/1.1; 	// ORANGE
-	_delay_ms(1000);
-		
-	OCR1B   = TOP/1.5; // YELLOW
-	_delay_ms(1000);
-	
-	OCR1B   = TOP/3; //GREEN
-	GREEN_light();
-	
-	_delay_ms(1000);
-	
-	BLUE_light();
-	OCR1B   = TOP/10; //INDIGO
-	_delay_ms(1000);
-	
-	OCR1B   = TOP/3; //VIOLET
-	_delay_ms(1000);
-	
-	TCCR1A = 0;
-	TCCR1B = 0;
-	OCR1A  = 0;
-	
-	TCCR1B &= ~_BV(CS11);
-	TCCR1B &= ~_BV(CS10);
-	
-	TIMSK  &=~_BV(OCIE1A);
-
-	DDRD |=_BV(RED) | _BV(GREEN) | _BV(BLUE); //RGB PB2 PB1 PB0
-	PORTD|=_BV(RED) | _BV(GREEN) | _BV(BLUE);
-	//PORTD|=_BV(GREEN) | _BV(BLUE);
-	
-	sei();
-}
-
-void money_check(){
-	
-	//ATD*100# 
-	//http://stackoverflow.com/questions/22238243/gsm-atd-command-to-check-my-balance
-	//AT+CUSD=[<n>[,<str>[,<dcs>]]]
-	char money[]="AT+CUSD=[<n>[,<str>[,<dcs>]]]";
-
-	refresh_rxBuffer();
-
-	USART_puts(money);
-	USART_putc(34); //Enter
-	//USART_putc(26); // CTRL+z
-	_delay_ms(3000);
-
-	see_rxBuffer();
-}
-void pay(){
-	
-}
 int main(void)
 {
 	
 	init();
 	rainbow();
 
-	//sei();
+	sei();
 
 
 	while(!enable_text_mode());
@@ -387,6 +333,34 @@ int main(void)
 	}
 }
 
+void init(){
+	//initialize IO
+	lcd_init(LCD_DISP_ON);
+	lcd_clrscr();
+	//backlight pins
+	DDRD |=_BV(RED) | _BV(GREEN) | _BV(BLUE); //RGB PB2 PB1 PB0
+	PORTD|=_BV(RED) | _BV(GREEN) | _BV(BLUE);
+	
+	
+	//initialize UART
+	//set baud rate
+	UBRRH = 0;
+	UBRRL = BRC;
+	//enable receiver and transmitter
+	UCSRB = (1 << RXEN) | (1 << TXEN)| (1<<RXCIE)| (1<<TXCIE);
+	//set frame format: 8 bit, character size 8 bit
+	UCSRC = (1 << UCSZ0) | (1 << UCSZ1)|(1 << URSEL);
+	
+	//initialize ADC
+	//adc enable, prescaler=64 -> clk=115200
+	ADCSRA = _BV(ADEN)|_BV(ADPS2)|_BV(ADPS1);
+	//AVcc reference voltage
+	ADMUX = _BV(REFS0);
+
+	//PA2 RED LED
+	DDRA |= _BV(LED); //LED
+}
+
 int enable_text_mode(void){
 
 	char text_mode[] = "AT+CMGF=1";
@@ -431,8 +405,6 @@ int enable_text_mode(void){
 		return 0;
 	}
 }
-
-
 
 int read_rxBuffer(void){
 
@@ -555,6 +527,7 @@ void reboot(){
 	_delay_ms(1000);
 	
 }
+
 void sleep_mode(){
 	
 	char sleep[]="AT+S32K";
@@ -661,7 +634,7 @@ void delete_sms(char index, char tenner){
 		_delay_ms(1000);
 	}
 }
-//ISPRAVITI !
+
 int get_from_number(int read){
 	
 	lcd_clrscr();
@@ -698,6 +671,7 @@ volatile uint8_t getLight(uint8_t channel)
 	while (ADCSRA & _BV(ADSC) );
 	return ADCH;
 }
+
 void LUX(){
 	//sms define
 	char sms[]="AT+CMGS="; //sms command
@@ -838,3 +812,149 @@ void LUX(){
 	}
 
 }
+
+void refresh_rxBuffer(){
+	*rxBuffer='\0';
+	rxReadPos=0;
+	rxWritePos=0;
+}
+
+void see_rxBuffer(){
+
+	lcd_clrscr();
+	rxReadPos=0;
+	
+	while(rxReadPos!=rxWritePos){
+		//if(rxReadPos%16==0 || rxReadPos==0) lcd_gotoxy(0,0);
+		if (rxReadPos==0) lcd_gotoxy(0,0);
+		else if (rxReadPos==16 || rxReadPos==48) lcd_gotoxy(0,1);
+		else if (rxReadPos==32 || rxReadPos==64) {
+			lcd_clrscr();
+			lcd_gotoxy(0,0);
+		}
+		lcd_putc(rxBuffer[rxReadPos]);
+		rxReadPos++;
+		_delay_ms(175);
+	}
+}
+
+void send_sms(char number[], char sms_text[]){
+	
+	char AT_send_sms[]="AT+CMGS=";
+	
+	refresh_rxBuffer();
+	
+	USART_puts(AT_send_sms);
+	USART_putc(34);
+	USART_puts(number);
+	USART_putc(34);
+	_delay_ms(1000);
+	USART_putc(13);
+	_delay_ms(3000);
+	see_rxBuffer();
+	_delay_ms(3000);
+	USART_puts(sms_text);
+	USART_putc(26); // CTRL+z
+	_delay_ms(3000);
+	
+	see_rxBuffer();
+}
+
+void _4_sms_test(){
+
+	char p_sms[]="AT+CMGS=";
+	//char p_number[]="385976737211";
+	char p_number[]="385996834050";
+
+	char p_text[]="LUX?";
+	char p_text2[]="LUKS?";
+	char p_text3[]="DUGA?";
+	char p_text4[]="DA";
+
+
+	send_sms(p_number,p_text);
+	send_sms(p_number,p_text2);
+	send_sms(p_number,p_text3);
+	send_sms(p_number,p_text4);
+}
+
+void rainbow(){
+
+
+	//pink: blue+ pwm red TOP/2 (Barbie) ili TOP/1.1 (fuksija)
+	//purple: blue+ pwm red TOP/3.5
+	//yello: green+ pwm red TOP/1.5
+	//orange: green + pwm red TOP/1.2 ili 1.1
+
+	
+	
+	cli();
+	
+	TCCR1A |= _BV(COM1B1)|_BV(WGM11)|_BV(WGM10);
+	TCCR1B |= _BV(WGM13)|_BV(WGM12);
+	OCR1A   = TOP;				// set TOP value
+	OCR1B   = RED_DEF;		// set default contrast value
+	
+	TIMSK  |= _BV(OCIE1A);
+
+	TCCR1B |= _BV(CS11) | _BV(CS10);
+	
+	RED_light();		//RED
+	_delay_ms(1000);
+	
+	GREEN_light();
+	OCR1B   = TOP/1.1; 	// ORANGE
+	_delay_ms(1000);
+	
+	OCR1B   = TOP/1.5; // YELLOW
+	_delay_ms(1000);
+	
+	OCR1B   = TOP/3; //GREEN
+	GREEN_light();
+	
+	_delay_ms(1000);
+	
+	BLUE_light();
+	OCR1B   = TOP/10; //INDIGO
+	_delay_ms(1000);
+	
+	OCR1B   = TOP/3; //VIOLET
+	_delay_ms(1000);
+	
+	TCCR1A = 0;
+	TCCR1B = 0;
+	OCR1A  = 0;
+	
+	TCCR1B &= ~_BV(CS11);
+	TCCR1B &= ~_BV(CS10);
+	
+	TIMSK  &=~_BV(OCIE1A);
+
+	DDRD |=_BV(RED) | _BV(GREEN) | _BV(BLUE); //RGB PB2 PB1 PB0
+	PORTD|=_BV(RED) | _BV(GREEN) | _BV(BLUE);
+	//PORTD|=_BV(GREEN) | _BV(BLUE);
+	
+	sei();
+}
+
+/*
+void money_check(){
+	
+	//ATD*100# 
+	//http://stackoverflow.com/questions/22238243/gsm-atd-command-to-check-my-balance
+	//AT+CUSD=[<n>[,<str>[,<dcs>]]]
+	char money[]="AT+CUSD=[<n>[,<str>[,<dcs>]]]";
+
+	refresh_rxBuffer();
+
+	USART_puts(money);
+	USART_putc(34); //Enter
+	//USART_putc(26); // CTRL+z
+	_delay_ms(3000);
+
+	see_rxBuffer();
+}
+void pay(){
+	
+}
+*/
